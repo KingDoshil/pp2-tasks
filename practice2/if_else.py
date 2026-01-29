ee6f1d0b8aacd2b9036b5151bfb5b74f708b67a5
Askhab Magomadov <a_magomadov@kbtu.kz>
2026-01-29 11:40:08 +0500

    Practice tasks

---
+++ practice2/if_else.py
@@ -0,0 +1,42 @@
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+  number = 15
+if number > 0:
+  print("The number is positive")
+  
+
+
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+  a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
+
+
+  x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
+
+    a = 33
+b = 200
+
+if b > a:
+  pass
